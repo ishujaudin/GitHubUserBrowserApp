2fbc38ed6f0584a2286349b05ba9bf19934a6805
Shuja Ud Din <shuja.uddin@beyonmoney.com>
2025-04-27 12:58:09 +0500

    Initial Commit

---
+++ GitHubUserBrowserApp/GitHubUserBrowserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUserBrowserAppApp.swift
+//  GitHubUserBrowserApp
+//
+//  Created by ShujaMBP on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUserBrowserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubUserBrowserAppTests/GitHubUserBrowserAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUserBrowserAppTests.swift
+//  GitHubUserBrowserAppTests
+//
+//  Created by ShujaMBP on 27/04/2025.
+//
+
+import Testing
+@testable import GitHubUserBrowserApp
+
+struct GitHubUserBrowserAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
